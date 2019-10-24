8dd4151c4bdf998a873d49a82a218a61ec336315
HumayunBA <humayun.bayani@gmail.com>
2019-10-24 16:37:16 +0200

    neating

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var listOfProduct
 var counter=0;
 
-/** Get products from the json file and store it in a gobal variable */
+
 function loadProducts() {
     fetch("./products.json")
     .then(function(response) {
@@ -13,29 +13,17 @@ function loadProducts() {
     });
 }
 
-
 function initSite() {
     localStorage.clear()
       loadProducts();
      
-        
-    // This would also be a good place to initialize other parts of the UI
 }
 
-/** Uses the loaded products data to create a visible product list on the website */
+
 function addProductsToWebpage() {
 
     setHeader()
-    // Add your code here, remember to brake your code in to smaller function blocks
-    // to reduce complexity and increase readability. Each function should have
-    // an explainetory comment like the one for this function, see row 22.
     
-    // TODO: Remove the console.log and these comments when you've read them.
-    
-
-
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    //console.log(listOfProducts);
     var main = document.getElementsByTagName("main")[0]
 
         var container = document.createElement("div")
@@ -141,12 +129,6 @@ function setHeader(){
       counter++;
   }
 
-
-
-//localStorage.setItem("names", JSON.stringify(names));
-
-
-//var storedNames = JSON.parse(localStorage.getItem("names"));
 
 function addToCart(product){ 
     if (!localStorage.getItem("shoppingList")) {
